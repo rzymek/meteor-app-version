f0fcafddc0e9f1e401b0ed70c8cf05e0041d9390
rzymek <rzymek@gmail.com>
2015-02-23 12:28:57 +0100

    packege meta-data

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
     name: 'rzymek:app-version',
     version: '0.0.1',
-    // Brief, one-line summary of the package.
-    summary: '',
-    // URL to the Git repository containing the source code for this package.
-    git: '',
-    // By default, Meteor will default to using README.md for documentation.
-    // To avoid submitting documentation, set this field to null.
+    summary: 'Generates public/VERSION.txt file based on git describe --tags',
+    git: 'https://github.com/rzymek/meteor-app-version.git',
     documentation: 'README.md'
 });
 
